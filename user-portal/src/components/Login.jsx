2120e80c699217b9c59426049b6f5072bb111fdb
sam <asirsam@rishoncomm.in>
2024-02-12 21:13:08 +0530

    Added State Function for the Form input fields

--- user-portal/src/components/Login.jsx
+++ user-portal/src/components/Login.jsx
@@ -1,6 +1,21 @@
-//import { useState } from 'react'
+import { useState } from 'react'
 import '../styles/login.css'
 function Login() {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const submit = (event) => {
+    event.preventDefault()
+  }
+
+  const handleChange = (e) => {
+
+    if(e.target.type === 'password')  setPassword(e.target.value);
+    else  setEmail(e.target.value);
+
+  };
+
 
   return (
     <div className="form-container">
@@ -9,10 +24,10 @@ function Login() {
       <h3 className='formheader'><center>Login in to User Management</center></h3>
       <div className='form-components'>
         <label >Email</label>
-        <input type="text" className='emailInput' name='email' placeholder = 'asirmsamdm@gmail.com' />
+        <input type="text" value={email} onChange={handleChange} className='emailInput' name='email' placeholder = 'asirmsamdm@gmail.com' />
         <label >Password</label>
-        <input type="password" className='loginpass' name='password' />
-        <div><center><button className='submit'>SEND</button></center></div>
+        <input type="password" value={password} onChange={handleChange} className='loginpass' name='password' />
+        <div><center><button onClick={submit} className='submit'>SEND</button></center></div>
       </div>
       </div>
       </form>
